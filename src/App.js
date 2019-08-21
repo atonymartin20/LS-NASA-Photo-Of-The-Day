0461ff501069c44bed17c996ae36d63955408e7e
Alex Martin <atonymartin20@yahoo.com>
2019-08-20 23:08:39 -0500

    Began adding NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import NavBar from './components/navbar/index.js';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
---
+++ src/components/navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./index.css";
+
+const NavBar = () => {
+    return (
+        <div className='nav-bar'>
+            Navbar
+        </div>
+    )
+}
+
+export default NavBar;
