f660d96632753179f8d88997af0f4695ed58d99e
Alex Martin <atonymartin20@yahoo.com>
2019-08-25 17:19:51 -0500

    Continued Testing

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
         <div className='nav-bar'>
             <span>Add Logo Here</span>
-            {/* <span>Test</span> */}
+            <span>Test</span>
             <span>Navbar</span>
         </div>
     )
