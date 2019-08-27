234ddb09943f457e46148600f9b81d47fdc5b206
Alex Martin <atonymartin20@yahoo.com>
2019-08-26 23:53:17 -0500

    More NavBar changes

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./index.css";
 import hamburger from '../../images/Hamburger_icon.png';
+import logo from '../../images/NASA-logo.png';
 
 const NavBar = () => {
     return (
         <div className='nav-bar'>
             <img src={hamburger} alt="Hamburger Icon" className="hamburger"/>
-            <img src={require('../../images/NASA-logo.png')} className="logo"/>
+            <img src={logo} alt="NASA logo" className="logo"/>
             <span>Something Here</span>
         </div>
     )
