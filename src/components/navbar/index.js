af29d6fd2b1f114a179bb456df6e9a416f813f05
Alex Martin <atonymartin20@yahoo.com>
2019-08-26 23:39:25 -0500

    Added Hamburger

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./index.css";
+import hamburger from '../../images/Hamburger_icon.png';
 
 const NavBar = () => {
     return (
         <div className='nav-bar'>
             <span>Hamburger Button</span>
-            <img src={require('../../images/NASA-logo.png')} />
+            <img src={require('../../images/NASA-logo.png')} className="logo"/>
+            <img src={hamburger} alt="Hamburger Icon" className="hamburger"/>
             <span>Test</span>
             <span>Navbar</span>
         </div>
