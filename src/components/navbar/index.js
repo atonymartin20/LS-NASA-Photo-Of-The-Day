6058638dab46e26cd0cbc967b97d5fdd21ecd02a
Alex Martin <atonymartin20@yahoo.com>
2019-08-28 14:27:09 -0500

    Commit 3

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -9,7 +9,7 @@ const NavBar = () => {
             <img src={hamburger} alt="Hamburger Icon" className="hamburger"/>
             <img src={logo} alt="NASA logo" className="logo"/>
             <span>Something Here</span>
-            {/* Commit 2 */}
+            {/* Commit 3 */}
         </div>
     )
 }
