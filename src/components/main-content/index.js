65967e01a9bf73c0bab6619f34bde7a4b9ed3e90
Alex Martin <atonymartin20@yahoo.com>
2019-09-01 23:02:33 -0500

    Adding main page content

--- src/components/main-content/index.js
+++ src/components/main-content/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./index.css";
 
 const Main = () => {
+
+    const[photos, setPhotos] = useState([]);
+
+    const fetchPhoto = () => {
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+            .then(res => res.json())
+            .then(photos => {
+                console.log(photos);
+                if (photos.status === "error") {
+                    return;
+                }
+                setPhotos(photos);
+            })
+            .catch(err => console.log("noooo"));
+        };
+
+    useEffect( () => {
+        fetchPhoto();
+    }, []);
+
     return (
         <div className='main'>
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+            <p>
+                Read through the instructions in the README.md file to build your NASA
+                app! Have fun <span role="img" aria-label="rocket">🚀</span>!
+            </p>
+            {photos.map(photo => (
+                <img width="200" src={photo} key={photo} alt={photo} />
+            ))}
         </div>
     )
 }
