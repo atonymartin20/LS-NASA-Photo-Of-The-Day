eadb53f06f6716d9632b7fd56f467fb517ac96b2
Alex Martin <atonymartin20@yahoo.com>
2019-09-03 23:49:13 -0500

    Working on adding video support

--- src/components/main-content/index.js
+++ src/components/main-content/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import "./index.css";
 import axios from 'axios';
+import ReactPlayer from 'react-player';
 
 const Main = () => {
 
@@ -17,18 +18,31 @@ const Main = () => {
                 setPhotos(photos.data);
                 
             })
-            .catch(err => console.log("noooo"));
+            .catch(err => console.log("I broke again ..."));
         };
 
     useEffect( () => {
         fetchPhoto();
     }, []);
 
+    // const DisplayInfo = () => {
+    //     if(!photos.hdurl) {
+    //         <ReactPlayer url={photos.url} playing width='50%' className="video-player" />
+    //     }
+    //     else {
+    //         <img src={photos.url} key={photos.date} alt={photos.title} className="POTD" />
+    //     }
+    // }
+
     return (
         <div className='main'>
             <h1>
                 NASA Photo of the Day
             </h1>
+            {/* {this.DisplayInfo} */}
+            if (photos.photos.pants) {
+                <ReactPlayer url={photos.url} playing width='50%' className="video-player" />
+            }
             <img src={photos.url} key={photos.date} alt={photos.title} className="POTD" />
         </div>
     )
