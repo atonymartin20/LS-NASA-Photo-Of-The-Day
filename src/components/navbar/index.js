4de7d3bbc6a6c0b27d92434958b6f763b4dd3383
Alex Martin <atonymartin20@yahoo.com>
2019-09-05 17:51:46 -0500

    Removed comments and navbar css file

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components'
-
-// import "./index.css";
 import hamburger from '../../images/Hamburger_icon.png';
 import logo from '../../images/NASA-logo.png';
 
@@ -37,36 +35,8 @@ const NavBar = () => {
         <NavBarCont>
             <HamburgerImg src={hamburger} alt="Hamburger Icon" className="hamburger"/>
             <LogoImg src={logo} alt="NASA logo" className="logo"/>
-            {/* <img src={hamburger} alt="Hamburger Icon" className="hamburger"/> */}
-            {/* <img src={logo} alt="NASA logo" className="logo"/> */}
         </NavBarCont>
     )
 }
 
-// .nav-bar {
-//     width: 100%;
-//     max-width: 1200px;
-//     background-color: #00baff;
-//     height: 50px;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     font-size: 1.5rem;
-//     padding: 0px 5%;
-//     box-sizing: border-box;
-//     position: fixed;
-//     top: 0;
-// }
-
-// .logo {
-//     width: 100%;
-//     max-width: 100px;
-//     margin-right: auto;
-// }
-
-// .nav-bar .hamburger {
-//     width: 100%;
-//     max-width: 50px;
-//     margin-right: auto;
-// }
 export default NavBar;
