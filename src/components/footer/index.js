cd184cc4fa33dc3b044ce3cbde1273469e929523
Alex Martin <atonymartin20@yahoo.com>
2019-09-05 17:36:23 -0500

    Added styled components in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import "./index.css";
+import styled from 'styled-components'
 
+const Footer = styled.footer`
+    width: 100%;
+    background-color: #C2c7c0;
+    color: #177245;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    padding: 0px 5%;
+    box-sizing: border-box;
+    margin-bottom: 20px;
+`;
 const Footer = () => {
     return (
-        <div className='footer'>
+        <Footer>
             <p>
                 Contact us at:
                 <br />NASA Headquarters
@@ -12,7 +25,7 @@ const Footer = () => {
                 <br />(202) 358-0001 (Office)
                 <br />(202) 358-4338 (Fax)
             </p>
-        </div>
+        </Footer>
     )
 }
 
