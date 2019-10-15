7c79e571b2824b970e57fc4ae17125f2d9ad1e63
Axis <atonymartin20@yahoo.com>
2019-10-14 23:54:04 -0500

    DC3

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -23,7 +23,7 @@ const Footer = () => {
                 <br />Washington, DC 20546
                 <br />(202) 358-0001 (Office)
                 <br />(202) 358-4338 (Fax)
-                {/* Commit 2 */}
+                {/* Commit 3 */}
             </p>
         </FooterCont>
     )
